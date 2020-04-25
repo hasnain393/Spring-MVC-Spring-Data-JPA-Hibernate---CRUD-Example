77fa8b14a0c1f3caff7539399b8a7966c2274b22
hasnain393 <56108097+hasnain393@users.noreply.github.com>
2020-04-25 20:43:59 +0530

    Add files via upload

---
+++ CustomerManager/CustomerManager/src/net/codejava/customer/Customer.java
@@ -0,0 +1,65 @@
+package net.codejava.customer;
+ 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+ 
+@Entity
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long id;
+	
+	private String name;
+	private String email;
+	private String address;
+
+	protected Customer() {
+    }
+ 
+    protected Customer(String name, String email, String address) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+    }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+ 
+    // getters and setters are not shown for brevity
+    
+ 
+}
